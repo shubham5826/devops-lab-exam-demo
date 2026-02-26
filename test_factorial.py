2fe40b29c2ba9f801cbe404282a5e5937b883e54
shubham5826 <shubhamsingh005826@gmail.com>
2026-02-26 11:36:01 +0530

    Add test cases for factorial of 0 and 1

--- test_factorial.py
+++ test_factorial.py
@@ -6,5 +6,11 @@ class TestFactorial(unittest.TestCase):
     def test_factorial_5(self):
         self.assertEqual(factorial(5), 120)
 
+    def test_factorial_0(self):
+        self.assertEqual(factorial(0), 1)
+
+    def test_factorial_1(self):
+        self.assertEqual(factorial(1), 1)
+
 if __name__ == "__main__":
     unittest.main()
